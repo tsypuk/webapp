45bac4b04a44873a3851aa762884d7a4e26ec05c
Roman Tsypuk <tsypuk.rb@gmail.com>
2016-05-01 15:34:31 +0300

    Added request listener

--- src/main/java/smartjava/RequestListener.java
+++ src/main/java/smartjava/RequestListener.java
@@ -1,7 +1,27 @@
 package smartjava;
 
+import javax.servlet.ServletRequestEvent;
+import javax.servlet.ServletRequestListener;
+import javax.servlet.annotation.WebListener;
+import java.util.Enumeration;
+
 /**
  * Created by roman.tsypuk on 5/1/16.
  */
-public class RequestListener {
+@WebListener
+public class RequestListener implements ServletRequestListener {
+    @Override
+    public void requestDestroyed(ServletRequestEvent sre) {
+        System.out.println("Request destroyed");
+    }
+
+    @Override
+    public void requestInitialized(ServletRequestEvent sre) {
+        System.out.println(sre.toString());
+        Enumeration<String> en = sre.getServletRequest().getParameterNames();
+        while (en.hasMoreElements()) {
+            String current = en.nextElement();
+            System.out.printf(sre.getServletRequest().getParameter(current));
+        }
+    }
 }
